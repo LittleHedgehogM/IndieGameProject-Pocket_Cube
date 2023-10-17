de8090308c41b7a4a97f085d69d93665ccadd599
Quesera008 <44633291+Quesera008@users.noreply.github.com>
2023-10-17 19:40:45 +0800

    Add: Level Manager

---
+++ PocketCubeGamePlay/Assets/Scripts/SceneManage/LevelManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance;
+
+    [SerializeField] private GameObject _loaderCanvas;
+    [SerializeField] private Image _progressBar;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    public async void LoadScene(string sceneName)
+    {
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = false;
+
+        _loaderCanvas.SetActive(true);
+
+        do{
+            await Task.Delay(100);
+
+            _progressBar.fillAmount = scene.progress;
+        }while (scene.progress < 0.9f);
+
+        scene.allowSceneActivation = true;
+        _loaderCanvas.SetActive(false);
+    }
+}
