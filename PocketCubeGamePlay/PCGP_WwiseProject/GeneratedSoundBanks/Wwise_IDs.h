a17568cc1c21a2a6f578c60601046bf730fe2af2
2250346489 <149752515+2250346489@users.noreply.github.com>
2023-11-22 12:09:22 +0800

    音频bank调整，level0的unlock01音频制作

--- PocketCubeGamePlay/PCGP_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ PocketCubeGamePlay/PCGP_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,6 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_AMB_LEVEL00 = 2850739289U;
-        static const AkUniqueID PLAY_AMB_MAIN = 1871703286U;
         static const AkUniqueID PLAY_BOX_CLICK = 1291434436U;
         static const AkUniqueID PLAY_CAMERA_ZOOMIN = 977134866U;
         static const AkUniqueID PLAY_CAMERA_ZOOMOUT = 2892692845U;
@@ -28,7 +26,6 @@ namespace AK
         static const AkUniqueID PLAY_CUBE_ROLL = 2377298701U;
         static const AkUniqueID PLAY_CUBE_SKILLCLICK01 = 3603099912U;
         static const AkUniqueID PLAY_CUBE_SKILLCLICK02 = 3603099915U;
-        static const AkUniqueID PLAY_ENVIROMENT = 108392005U;
         static const AkUniqueID PLAY_FINAL = 540950438U;
         static const AkUniqueID PLAY_HOVER = 1962302600U;
         static const AkUniqueID PLAY_JIQIREN = 1467814842U;
@@ -43,7 +40,6 @@ namespace AK
         static const AkUniqueID PLAY_MID_UNLOCK = 928358091U;
         static const AkUniqueID PLAY_MOVE_LOOP = 3382199950U;
         static const AkUniqueID PLAY_NEWTON_ENVIRONMENT = 2419270649U;
-        static const AkUniqueID PLAY_OPENING = 1808131806U;
         static const AkUniqueID PLAY_PLATE_DOWN = 3551052341U;
         static const AkUniqueID PLAY_PLATE_LIFT = 731342154U;
         static const AkUniqueID PLAY_REDUCE = 3470104452U;
@@ -52,8 +48,7 @@ namespace AK
         static const AkUniqueID PLAY_SHINE = 3162051019U;
         static const AkUniqueID PLAY_SIDE_UNLOCK01 = 2931672207U;
         static const AkUniqueID PLAY_SIDE_UNLOCK02 = 2931672204U;
-        static const AkUniqueID STOP_AMB_LEVEL00 = 352035487U;
-        static const AkUniqueID STOP_AMB_MAIN = 1557599088U;
+        static const AkUniqueID PLAY_UNLOCK01 = 4083196823U;
         static const AkUniqueID STOP_LEVEL3_MUSIC = 3042334185U;
         static const AkUniqueID STOP_MERRYGOROUNDMUSIC = 3379697116U;
         static const AkUniqueID STOP_MOVE_LOOP = 1423719756U;
@@ -70,12 +65,14 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMB = 1117531639U;
         static const AkUniqueID CUBE = 4052031814U;
         static const AkUniqueID LEVEL0 = 2678230383U;
         static const AkUniqueID LEVEL1 = 2678230382U;
         static const AkUniqueID LEVEL2 = 2678230381U;
         static const AkUniqueID LEVEL3 = 2678230380U;
         static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID PLAYER = 1069431850U;
         static const AkUniqueID UI = 1551306167U;
     } // namespace BANKS
