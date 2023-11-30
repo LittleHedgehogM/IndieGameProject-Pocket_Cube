e49ad374962c2e601517f1f4ba617afc9d15feca
Quesera008 <44633291+Quesera008@users.noreply.github.com>
2023-11-30 13:00:48 +0800

    动画transition

--- PocketCubeGamePlay/Assets/Scripts/Audio/VideoScript.cs
+++ PocketCubeGamePlay/Assets/Scripts/Audio/VideoScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 
 public class VideoScript: MonoBehaviour
@@ -11,7 +12,7 @@ public class VideoScript: MonoBehaviour
     //private bool isOpeningPlayed = false;
     public static Action VideoFinished;
     [SerializeField] GameObject performCamera;
-
+    [SerializeField] float translationTime = 1f;
     void Start()
     {
         vid = GetComponent<VideoPlayer>();
@@ -23,9 +24,37 @@ public class VideoScript: MonoBehaviour
     // Update is called once per frame
     void DestroyAfterVideoPlayed(VideoPlayer vp)
     {
-        gameObject.SetActive(false);
+        if (SceneManager.GetActiveScene().name == "First GPP")
+        {
+            gameObject.SetActive(false);
+            performCamera.SetActive(true);
+            VideoFinished?.Invoke();
+        }
+        else if (SceneManager.GetActiveScene().name == "NewtonLevel_GPP_Test")
+        {
+            StartCoroutine(VideoFadeout());
+        }
+        
+    }
+
+    private IEnumerator VideoFadeout()
+    {
+        float startAlphaVal = 1;
+        float targetAlphaVal = 0;
+        float currentTime = 0;
+        
         performCamera.SetActive(true);
+        vid.targetCamera = performCamera.GetComponent<Camera>();
+        while (vid.targetCameraAlpha > 0)
+        {
+            currentTime += Time.deltaTime;
+            
+            vid.targetCameraAlpha = Mathf.Lerp(startAlphaVal, targetAlphaVal, translationTime * currentTime);
+            yield return null;
+        }
+        //finishImage.gameObject.SetActive(false);
+        gameObject.SetActive(false);
         VideoFinished?.Invoke();
-        // play camera 
+        yield return null;
     }
 }
